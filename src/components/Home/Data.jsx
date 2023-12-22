9a7c4afb32e42d802e4e3a4824905723f4079ce2
Eksal Pujianto <eksalpu0@gmail.com>
2023-12-22 18:04:01 +0700

    iinfoo

--- src/components/Home/Data.jsx
+++ src/components/Home/Data.jsx
@@ -9,7 +9,7 @@ const Data = () => {
       </h1>
       <TulisanGerak />
 
-      <p className="home__description">Web Developer Enthusiast | Front End Engineer</p>
+      <p className="home__description">Business Enthusiast | Finance Student</p>
       <a href="#contact" className="anjay button button--flex">
         Contact Me!
         <svg class="button__icon animate" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
